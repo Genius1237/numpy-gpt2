Fix softmax, add semi working attention implementation

model.py
@@ -151,8 +151,45 @@ class Attention(Base):
         self.n_embd = n_embd
         self.n_head = n_head
 
-        self.c_attn = Linear(self.n_embd, 3 * self.n_head)
+        self.c_attn = Linear(self.n_embd, 3 * self.n_embd)
         self.c_proj = Linear(self.n_embd, self.n_embd)
+    
+    def __call__(self, inputs):
+        c_attn = self.c_attn(inputs)
+        batch_size = inputs.shape[0]
+        seq_length = inputs.shape[1]
+
+        causal_mask =  np.tile(np.expand_dims(np.triu(np.ones(seq_length), k=1), (0, 1)), ((batch_size, self.n_head, 1, 1)))
+
+        # c_attn_reshaped = c_attn.reshape(batch_size, seq_length, self.n_head, 3, self.n_embd // self.n_head).transpose(3, 0, 2, 1, 4)
+        # q, k, v  = c_attn_reshaped
+        # c_attn_reshaped = c_attn.reshape(batch_size, seq_length, self.n_head, self.n_embd // self.n_head, 3).transpose(4, 0, 2, 1, 3)
+        # q, k, v = c_attn_reshaped
+        # c_attn_reshaped = c_attn.reshape(batch_size, seq_length, 3, self.n_embd // self.n_head, self.n_head).transpose(2, 0, 4, 1, 3)
+        # q, k, v = c_attn_reshaped
+        c_attn_reshaped = c_attn.reshape(batch_size, seq_length, 3, self.n_head, self.n_embd // self.n_head).transpose(2, 0, 3, 1, 4)
+        q, k, v = c_attn_reshaped
+        # c_attn_reshaped = c_attn.reshape(batch_size, seq_length, self.n_embd // self.n_head, 3, self.n_head).transpose(3, 0, 4, 1, 2)
+        # q, k, v = c_attn_reshaped
+        # c_attn_reshaped = c_attn.reshape(batch_size, seq_length, self.n_embd // self.n_head, self.n_head, 3).transpose(4, 0, 3, 1, 2)
+        # q, k, v = c_attn_reshaped
+        # (b, h, l, d)
+
+        matmul = np.matmul(q, k.transpose(0, 1, 3, 2)) / np.sqrt(self.n_embd // self.n_head)
+        matmul = np.ma.array(matmul, mask=causal_mask).filled(fill_value=-np.inf)
+        weights = softmax(matmul, axis=-1)
+        # (b, h, l, l)
+
+        # weights =  np.tile(np.expand_dims(np.eye(seq_length), (0, 1)), ((batch_size, self.n_head, 1, 1)))
+        weighted_sum = (np.expand_dims(v, 3).repeat(seq_length, axis=3) * np.expand_dims(weights, 4).repeat(self.n_embd // self.n_head, axis=4)).sum(axis=3)
+        # (b, h, l, d)
+
+        # assert np.allclose(v, weighted_sum, atol=1e-5)
+
+        concatenated_weighted_sum=weighted_sum.transpose(0,2,1,3).reshape(batch_size, seq_length, -1)
+
+        return self.c_proj(concatenated_weighted_sum), weights
+
 
 class Embedding(Base):
     def __init__(self, vocab_size, n_embd):
@@ -167,12 +204,13 @@ class Embedding(Base):
 def sigmoid(inp):
     return 1 / (1 + np.exp(-inp))
 
+def softmax(inp, axis=-1):
+    inp = inp - np.max(inp, axis=axis, keepdims=True)
+    inp_exp = np.exp(inp)
+    return inp_exp / np.sum(inp_exp, axis=axis, keepdims=True)
 
 def gelu(inp):
     # return inp * sigmoid(1.702 * inp)
     # return inp * phi(inp)
     # return 0.5 * inp * (1.0 + np.tanh(np.sqrt(2.0/np.pi) * (inp + 0.044715 * inp**3)))
     return 0.5 * inp * (1.0 + erf(inp / np.sqrt(2.0)))
-
-
-

test_layers.py
@@ -4,7 +4,7 @@ import numpy as np
 import torch
 
 def get_models():
-    hf_model = transformers.AutoModelForCausalLM.from_pretrained("gpt2", output_attentions=True, output_hidden_states=True)
+    hf_model = transformers.AutoModelForCausalLM.from_pretrained("gpt2", output_hidden_states=True, output_attentions=True)
     params = {k: v.detach().numpy() for k,v in hf_model.named_parameters()}
 
     gpt2model = GPT2Model(hf_model.config)
@@ -134,5 +134,30 @@ def test_layernorm():
     np_out = gpt2model.h[0].ln_1(random_input.numpy())
     assert np.allclose(np_out, pt_out, atol=1e-5)
 
+def test_attention():
+    hf_model, gpt2model = get_models()
+
+    inputs = []
+    outputs = []
+    def hook_fn(m, i, o):
+        inputs.append(i)
+        outputs.append(o)
+
+    hf_model.transformer.h[0].attn.register_forward_hook(hook_fn)
+
+    batch_size = 32
+    max_len = 65
+    # random_input = torch.randn((batch_size, max_len, hf_model.config.n_embd))
+    # pt_out, pt_attention_weights = (t.detach().numpy() for t in hf_model.transformer.h[0].attn(random_input))
+    random_inputs = torch.randint(0, hf_model.config.vocab_size, (batch_size,max_len))
+    # random_inputs = torch.load("random_inputs.pt")
+    pt_attention_weights = hf_model(input_ids=random_inputs).attentions[0].detach().numpy()
+    pt_out = outputs[0][0].detach().numpy()
+
+    np_out, np_attention_weights = gpt2model.h[0].attn(inputs[0][0].detach().numpy())
+    print(pt_attention_weights[0][0], np_attention_weights[0][0])
+    assert np.allclose(pt_attention_weights, np_attention_weights, atol=1e-5)
+    assert np.allclose(pt_out, np_out, atol=1e-5)
+
 if __name__ == "__main__":
-    test_linear()
+    test_attention()